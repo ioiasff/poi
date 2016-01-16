800032633e893e003d3e0f10aa8c4078e252d048
ioiasff <arash.github@gmail.com>
2016-01-16 19:35:43 +0330

    Create link.lua

---
+++ plugins/link.lua
@@ -0,0 +1,51 @@
+   do
+--link by arash--
+ if matches[1] == 'newlink' and not is_realm(msg) then
+      if not is_momod(msg) then
+        return "For moderators only!"
+      end
+      local function callback (extra , success, result)
+        local receiver = 'chat#'..msg.to.id
+        if success == 0 then
+           return send_large_msg(receiver, '*Error: Invite link failed* \nReason: Not creator.')
+        end
+        send_large_msg(receiver, "Created a new link")
+        data[tostring(msg.to.id)]['settings']['set_link'] = result
+        save_data(_config.moderation.data, data)
+      end
+      local receiver = 'chat#'..msg.to.id
+      savelog(msg.to.id, name_log.." ["..msg.from.id.."] revoked group link ")
+      return export_chat_link(receiver, callback, true)
+    end
+    if matches[1] == 'link' then
+      if not is_momod(msg) then
+        return "For moderators only!"
+      end
+      local group_link = data[tostring(msg.to.id)]['settings']['set_link']
+      if not group_link then 
+        return "Create a link using /newlink first 🆕🔗"
+      end
+       savelog(msg.to.id, name_log.." ["..msg.from.id.."] requested group link ["..group_link.."]")
+      return "Group link🔗:\n"..group_link
+    end
+    if matches[1] == 'setowner' and matches[2] then
+      if not is_owner(msg) then
+        return "For owner only!"
+      end
+      data[tostring(msg.to.id)]['set_owner'] = matches[2]
+      save_data(_config.moderation.data, data)
+      savelog(msg.to.id, name_log.." ["..msg.from.id.."] set ["..matches[2].."] as owner")
+      local text = matches[2].." added as owner🌟"
+      return text
+    end
+  end 
+end
+
+return {
+  patterns = {
+"^[!/](newlink)$",
+  "^[!/](link)$",
+  },
+  run = run
+}
+end
