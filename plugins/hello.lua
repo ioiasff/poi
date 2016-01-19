2243d3a7f85f0e09f6459664a87b596cdfabbcba
ioiasff <arash.github@gmail.com>
2016-01-19 18:20:34 +0330

    Create hello.lua

--- plugins/hello.lua
+++ plugins/hello.lua
@@ -1,15 +1,15 @@
 do
 
 function run(msg, matches)
-  return "Hello, " .. matches[1]
+  return "➕, " .. matches[1]
 end
 
 return {
   description = "Says hello to someone", 
   usage = "say hello to [name]",
   patterns = {
-    "^say hello to (.*)$",
-    "^Say hello to (.*)$"
+    "^بگو (.*)$",
+    "^Say (.*)$"
   }, 
   run = run 
 }
