928dc1210aa29fbe8fa676526fab5690b841c81c
ioiasff <arash.github@gmail.com>
2016-01-19 20:51:06 +0330

    Create owners.lua

--- plugins/owners.lua
+++ plugins/owners.lua
@@ -1,4 +1,5 @@
 
+
 local function lock_group_namemod(msg, data, target)
   local group_name_set = data[tostring(target)]['settings']['set_name']
   local group_name_lock = data[tostring(target)]['settings']['lock_name']
